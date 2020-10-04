37e232ae9c5b8d32060507e3906c93d640e74097
vconte41982 <68178433+vconte41982@users.noreply.github.com>
2020-10-03 20:37:51 -0700

    update Javascript,screenshots and ReadME

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # popup_quiz
+
+This is a quiz to test the knowledge acquired at UC Berkeley bootcamp
+
+### Technologies
+
+This is the technologies, I used for this project:
+
+- Html
+- Css
+- JavaScript
+
+
+### How to use it
+
+in order to use please visit this link: https://github.com/vconte41982/popup_quiz
+
+### Screenshots
+
+![screenshot](/quiz_screenshots/screenshot1.png)
+![screenshot](/quiz_screenshots/screenshot2.png)
+![screenshot](/quiz_screenshots/screenshot3.png)
+![screenshot](/quiz_screenshots/screenshot4.png)
--- quiz.js
+++ quiz.js
@@ -1,8 +1,8 @@
-const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll(".choice-text"));
-const progressText = document.querySelector("#progresstext");
-const ScoreText = document.querySelector("#score");
-const progressBarFull = document.querySelector("#progressBarFull");
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const ScoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -13,32 +13,28 @@ let availableQuestions = [];
 let questions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
-    choice1: "script",
+    choice1: "<script>",
     choice2: "js",
     choice3: "scripting",
     choice4: "javascript",
     answer: 1,
   },
-
   {
-    question: "Where is the correct place to insert JavaScript",
+    question: "Where is the correct place to insert JavaScript?",
     choice1: "The body section",
     choice2: "The head section",
-    choice3: "The p section",
+    choice3: "The <p> section",
     choice4: "Both the head section and the body section",
     answer: 4,
   },
-
   {
-    question:
-      'What is the correct syntax for referring to an external script called "xxx.js"?',
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
     choice1: 'script name="xxx.js"',
     choice2: 'script src="xxx.js"',
     choice3: 'script href="xxx.js"',
     choice4: "Both the head section and the body section",
     answer: 2,
   },
-
   {
     question: 'How do you write "Hello World" in an alert box?',
     choice1: 'alert("Hello World");',
@@ -47,7 +43,6 @@ let questions = [
     choice4: 'alertBox("Hello World");',
     answer: 1,
   },
-
   {
     question: "How to create a function in JavaScript?",
     choice1: "function = myFunction()",
@@ -56,7 +51,6 @@ let questions = [
     choice4: "function()myFunction",
     answer: 2,
   },
-
   {
     question: "How does a WHILE loop start?",
     choice1: "while (i,<= 10; i++)",
@@ -65,10 +59,8 @@ let questions = [
     choice4: "while (i <= 1)0",
     answer: 3,
   },
-
   {
-    question:
-      'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
     choice1: "if i=! 5 then",
     choice2: "if(i<> 5)",
     choice3: "if (i!=5)",
@@ -83,18 +75,18 @@ startQuiz = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  getNewQuestion();
+  endQuiz();
 };
-
-getNewQuestion = () => {
+endQuiz = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("/end.html");
+    localStorage.setItem('mostRecentScore', score);
+
+    return window.location.assign('end.html');
   }
 
   questionCounter++;
   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-  progressBarFull.style.width = `${(questionCounter / Max_QUESTIONS) * 100}%`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionsIndex];
@@ -109,15 +101,14 @@ getNewQuestion = () => {
   acceptingAnswers = true;
 };
 choices.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
+  choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    let classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
     if (classToApply === "correct") {
       incrementScore(SCORE_POINTS);
@@ -127,14 +118,15 @@ choices.forEach((choice) => {
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
+      endQuiz();
     }, 1000);
   });
 });
 
-incrementScore = function (num) {
+incrementScore = (num) => {
   score += num;
   ScoreText.innerText = score;
 };
 
 startQuiz();
+
